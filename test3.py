a5365caf6021d64914a20e1ec2d8dfdcbd6d9a46
Andrew <andrewtawf@gmail.com>
2019-02-26 15:28:09 +0200

    Clean Up Mac OS Code

--- test3.py
+++ test3.py
@@ -1,48 +1,29 @@
-from PyObjCTools import AppHelper
-from Foundation import NSObject, NSKeyValueObservingOptionNew, NSKeyValueChangeNewKey
-from Foundation import NSRunLoop, NSDate
-from AppKit import NSApplication, NSApp, NSWorkspace
-
-from subprocess import Popen, PIPE
-import threading
+import sys
 import json
+import threading
+if sys.platform == "darwin":
+    from PyObjCTools import AppHelper
+    from Foundation import NSObject, NSKeyValueObservingOptionNew, NSKeyValueChangeNewKey
+    from Foundation import NSRunLoop, NSDate
+    from AppKit import NSApplication, NSApp, NSWorkspace
+    from subprocess import Popen, PIPE
 
 
-isBrowser = False
-localizedName = None
 active_url = None
 
 
-def getBrowserTabTitle():
-    global localizedName
-    global isBrowser
-    if not isBrowser:
-        return
-    script = active_url[localizedName]
-    p = Popen(['osascript', '-'], stdin=PIPE, stdout=PIPE,
-              stderr=PIPE, universal_newlines=True)
-    stdout, stderr = p.communicate(script)
-    print(stdout, stderr)
-    threading.Timer(1, getBrowserTabTitle).start()
-
-
 class Observer(NSObject):
+    def observe_(self, callback):
+        self.callback = callback
+        return self
+
     def observeValueForKeyPath_ofObject_change_context_(self, path, object, changeDescription, context):
-        global localizedName
-        global isBrowser
-        frontmostApplication = changeDescription[NSKeyValueChangeNewKey]
-        localizedName = frontmostApplication.localizedName()
-        bundleIdentifier = frontmostApplication.bundleIdentifier()
-        print (localizedName, bundleIdentifier)
-        isBrowser = False
-        if (localizedName in active_url):
-            isBrowser = True
-            getBrowserTabTitle()
+        self.callback(changeDescription[NSKeyValueChangeNewKey])
 
 
 class FrontMostAppObserver:
     def __init__(self):
-        self.observer = Observer.new()
+        self.observer = Observer.alloc().init().observe_(self.observerCallback)
         NSWorkspace.sharedWorkspace().addObserver_forKeyPath_options_context_(
             self.observer, "frontmostApplication", NSKeyValueObservingOptionNew, 0)
 
@@ -53,6 +34,25 @@ class FrontMostAppObserver:
         NSWorkspace.sharedWorkspace().removeObserver_forKeyPath_(
             self.observer, "frontmostApplication")
 
+    def observerCallback(self, frontmostApplication):
+        global active_url
+        self.localizedName = frontmostApplication.localizedName()
+        self.bundleIdentifier = frontmostApplication.bundleIdentifier()
+        self.isBrowser = True if self.localizedName in active_url else False
+        if (self.isBrowser):
+            self.getBrowserTabTitle()
+
+    def getBrowserTabTitle(self):
+        global active_url
+        if not self.isBrowser:
+            return
+        script = active_url[self.localizedName]
+        p = Popen(['osascript', '-'], stdin=PIPE, stdout=PIPE,
+                  stderr=PIPE, universal_newlines=True)
+        stdout, stderr = p.communicate(script)
+        print(stdout, stderr)
+        threading.Timer(1, self.getBrowserTabTitle).start()
+
 
 def loadJson():
     global active_url
@@ -61,10 +61,10 @@ def loadJson():
 
 
 def main():
-    loadJson()
-    with FrontMostAppObserver() as frontMostAppObserver:
-        AppHelper.runConsoleEventLoop(installInterrupt=True)
-
+    if sys.platform == "darwin":
+        loadJson()
+        with FrontMostAppObserver() as frontMostAppObserver:
+            AppHelper.runConsoleEventLoop(installInterrupt=True)
 
 if __name__ == '__main__':
     main()
